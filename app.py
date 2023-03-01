ca23b8bf0427740bb0e4981678c02f42b2943e81
Harish Karumuri <harishskarumuri@gmail.com>
2023-03-01 12:02:04 -0500

    added flickr pictures, cleaned up code, caught some exceptions

--- app.py
+++ app.py
@@ -10,40 +10,7 @@ import random
 import mood_to_food as moodToF
 import food_to_restaurant as fToRest
 
-
-# layout of the app 
-st.set_page_config(
-    page_title="Food Swings",
-    page_icon=":fork_and_knife:",
-    # layout="wide"
-)
-st.title('Food Swings')
-# left, right = st.columns(2)  
-st.sidebar.title("About")
-st.sidebar.caption("Food-Swings is a food recommendation application designed to cater to user\'s mood.") 
-st.sidebar.caption("It will make personalized recommendations of food and local restaurant based on the user\'s mood.")
-st.sidebar.title("Resources")
-st.sidebar.markdown("---")
-st.sidebar.markdown("DFP C3 Group2")
-
-
-# Define the mood to food mapping
-mood_to_food = moodToF.get_all_food_list()
-
-#identify mood
-userMood = random.choice(list(mood_to_food.keys()))
-
-# MAIN PAGE
-mood_text = st.text_input("How are you feeling today?",key="text", max_chars=100, placeholder="I'm Happy!")
-submit = st.button("Submit")
-
-if(submit):
-    user_input = mood_text.title()
-
-    st.write(f"#### You are feeling _{userMood}_")
-    st.write("Here are food options for your moods!")
-
-
+## - Helper function to find the first image in a webpage and return the image -
 # Send a GET request to the webpage and get the HTML content
 def get_main_img(url):
     response = requests.get(url)
@@ -62,34 +29,64 @@ def get_main_img(url):
             return img
             # st.image(img, caption='Website Image')
 
-if(submit):
-    # provide options to either select foods, recipes, restaurants
-    foods_tab, recipes_tab, restaurants_tab = st.tabs(["Food", "Recipe", "Restaurant"])
 
-    # mood_text will be later used as an user input to search!
+# Main app details setup
+st.set_page_config(
+    page_title="Food Swings",
+    page_icon=":fork_and_knife:",
+    # layout="wide"
+)
+st.title('Food Swings')
+# left, right = st.columns(2)  
+st.sidebar.title("About")
+st.sidebar.caption("Food-Swings is a food recommendation application designed to cater to user\'s mood.") 
+st.sidebar.caption("It will make personalized recommendations of food and local restaurant based on the user\'s mood.")
+st.sidebar.title("Resources")
+st.sidebar.markdown("---")
+st.sidebar.markdown("DFP C3 Group2")
+
+# temporary code for testing
+# Define the mood to food mapping
+mood_to_food = moodToF.get_all_food_list() #TODO temp line of code, delete later
+#identify mood
+userMood = random.choice(list(mood_to_food.keys()))  #TODO temp line of code, delete after integrating ml stuff
+
+# MAIN PAGE 
+# -- User can input text about how they are feeling
+mood_text = st.text_input("How are you feeling today?",key="text", max_chars=100, placeholder="I'm Happy!")
+submit = st.button("Submit")
+
+if(submit):
+    
+    # TODO impement userMood integration here
+    # userMood = somefunction(mood_text)
     food_list = moodToF.get_food_list(userMood)
     food_item = random.choice(food_list)
 
-    # Sample code that displays main image of the website if there is any
-    # Set the URL of the webpage you want to scrape
-    url = "https://www.icecream.com/"
+    #present how user is feeing
+    st.write(f"#### You are feeling _{userMood}_")
+    st.write("Here are food options for your moods!")
 
+    # provide options to either select foods, recipes, restaurants
+    foods_tab, recipes_tab, restaurants_tab = st.tabs(["Food", "Recipe", "Restaurant"])
 
+    #tab to show food reccomendation
     with foods_tab:
-        st.write(f"### You want....**{food_item}**!")
+        st.write(f"### You want....**{food_item.title()}**!")
+        # get an image of the food from flikr     
+        try: 
+            food_pic = get_main_img("https://www.flickr.com/search/?text=food%20" + food_item.replace(" ", "%20"))
+            st.image(food_pic)
+        except:
+            pass #print("image not found")
 
-
-        # st.image(get_main_img(url), width=200, caption=url)
-        #new_url = "https://www.aheadofthyme.com/40-best-pasta-recipes/"
-        #st.image(get_main_img(new_url), width=200, caption=new_url)
-
+    #tab to show a recipe for the food
     with recipes_tab:
         # display what goest in recipes_tab
         st.write("Recipe Ideas!")
         
-
+    #tab to show where the user can buy the food
     with restaurants_tab:
-        # display what goest in recipes_tab
         st.write("Where to get the food!")
         
         # Disply results when user submits the input
@@ -98,9 +95,13 @@ if(submit):
             url = results['link']
             st.write("*Here's an article that can help you find a place to get this food!*")
             st.write(results['title'])
-            if st.button('Open article'):
-                webbrowser.open_new_tab(url)
-            st.image(get_main_img(url), width=200, caption=url)
+            st.write(results["link"])
+            # if st.button('Open article'):  #TODO not working
+                # webbrowser.open_new_tab(url)
+            try:
+                st.image(get_main_img(url), width=200, caption=url) #TODO this line can get hung up (for example, if trying to get something from pizza hut). Is there a way to time out of a python function?
+            except:
+                pass #print("restaurant image not found")
 else:
     st.write("### Ready to find deliciousness! :yum:")
 
